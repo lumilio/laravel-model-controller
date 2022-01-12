9baba9b93e754361a7d74256837c545cfec470da
lumilio <logistic-division@lumilium.com>
2022-01-12 05:09:32 +0100

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        ddd(Movie::all());
+        return view('welcome');
+    }
+}
